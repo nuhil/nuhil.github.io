1023ba2bc4585bb20364645a08d7d299037e57ed
Nuhil Mehdy <1129785+nuhil@users.noreply.github.com>
2020-07-30 13:24:33 -0600

    Updated resume link

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const COMMANDS = {
     "<span class='success'>Master of Science - </span>Computer Science | Lamar University | Aug 2017 | Beaumont, TX, USA<br>" +
     "<span class='success'>Bachelor of Science and Engineering - </span>Computer Science and Engineering | RUET | Bangladesh",
   resume: 
-    "Please, follow <a href='http://bit.ly/akm_nuhil_mehdy' target='_blank' class='success link'>this link</a>",
+    "Please, follow <a href='downloads/AKM_Nuhil_Mehdy.pdf' target='_blank' class='success link'>this link</a>",
   experience: 
   '<span class="success">Data Science Intern - </span> Micron Technology, Inc. (May 2020 - Present | Boise, ID, USA)<br>' +
   '<span class="success">Graduate Research Assistant - </span> Boise State University (Jan 2018 - Present | Boise, ID, USA)<br>' +
